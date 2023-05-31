ff83c6b8f4fbd69a08dd6d49c05c926c4bf3637f
thaibaocoder1 <thaibaok4.1@gmail.com>
2023-05-31 17:22:53 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Navbar menu
+window.addEventListener("load", function () {
+  const navbarList = document.querySelector(".navbar-list");
+  const navbarMenu = document.querySelector(".navbar-menu");
+  const overlay = document.querySelector(".overlay");
+  navbarMenu.addEventListener("click", displayMenuMobile);
+  function displayMenuMobile(e) {
+    navbarList.classList.toggle("active");
+    overlay.style.display = "block";
+  }
+  document.body.addEventListener("click", function (e) {
+    if (e.target.matches(".overlay")) {
+      navbarList.classList.remove("active");
+      overlay.style.display = "none";
+    }
+  });
+});
+// Display submenu
+window.addEventListener("load", function () {
+  const elPage = document.getElementById("page");
+  const subnavMenu = this.document.querySelector(".subnav-mobile");
+  elPage.addEventListener("click", function () {
+    subnavMenu.classList.toggle("is-actived");
+  });
+});
